c881bb958fab984a175b4a7cfaf2d6610c1ff4e9
Lucas Triozon <lucas.triozon@orange.fr>
2025-03-10 18:53:09 +0100

    Calculator page

---
+++ src/lib/components/ui/search.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+
+import * as Select from "$lib/components/ui/select/index.ts";
+import { Label } from "$lib/components/ui/label/index.ts";
+import { Slider } from "$lib/components/ui/slider/index.ts";
+
+type Selects = {
+    value:string,
+    label:string
+}
+
+let soils:Selects[] = [
+    {value:"clay", label:"Argile"},
+    {value:"sand", label:"Sable"},
+    {value:"loam", label:"Terre"}
+]
+
+let roots:Selects[] = [
+    {value:"short", label:"Racines courtes"},
+    {value:"medium", label:"Racines moyennes"},
+    {value:"long", label:"Racines longues"},
+]
+
+let light:number[] = $state([50])
+let moisture:number[] = $state([50])
+let temperture:number[] = $state([50])
+
+</script>
+
+<div class="w-[300px]">
+    <div class="mb-5">
+        <div class="font-bold text-xl">Caractéristiques de la plante</div>
+        <div>Ces caractéristiques concernent la vie de la plante et de kjq...</div>
+    </div>
+
+    <div class="mb-3">
+        <div class="mb-1">Choix du sol</div>
+        <Select.Root portal={null} id="select">
+            <Select.Trigger class="w-[180px]">
+              <Select.Value placeholder="Choisir un sol" />
+            </Select.Trigger>
+            <Select.Content>
+              <Select.Group>
+                <Select.Label>Sols</Select.Label>
+                {#each soils as soil}
+                  <Select.Item value={soil.value} label={soil.label}
+                    >{soil.label}</Select.Item
+                  >
+                {/each}
+              </Select.Group>
+            </Select.Content>
+            <Select.Input name="favoriteFruit" />
+          </Select.Root>
+    </div>
+
+    <div>
+        <div class="mb-1">Type de racines</div>
+        <Select.Root portal={null} id="select">
+            <Select.Trigger class="w-[180px]">
+              <Select.Value placeholder="Choisir une racine" />
+            </Select.Trigger>
+            <Select.Content>
+              <Select.Group>
+                <Select.Label>Racines</Select.Label>
+                {#each roots as root}
+                  <Select.Item value={root.value} label={root.label}
+                    >{root.label}</Select.Item
+                  >
+                {/each}
+              </Select.Group>
+            </Select.Content>
+            <Select.Input name="favoriteFruit" />
+          </Select.Root>
+    </div>
+
+    <div class="mb-5 mt-5">
+        <div class="font-bold text-xl">Paramètres environnementaux</div>
+        <div>L'environnement correspond aux conditions de vie de la plante...</div>
+    </div>
+
+    <div class="mb-3">
+        <div class="mb-2">Luminosité - {light}</div>
+        <Slider bind:value={light} max={100} step={1} class="w-[180px]" />
+    </div>
+
+    <div class="mb-3">
+        <div class="mb-2">Humidité - {moisture}</div>
+        <Slider bind:value={moisture} max={100} step={1} class="w-[180px]" />
+    </div>
+
+    <div>
+        <div class="mb-2">Température - {temperture}</div>
+        <Slider bind:value={temperture} max={100} step={1} class="w-[180px]" />
+    </div>
+</div>
--- src/routes/calculator/+page.svelte
+++ src/routes/calculator/+page.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
+    import Results from "$lib/components/ui/results.svelte";
+    import Search from "$lib/components/ui/search.svelte";
+
+    import Tree from "$lib/assets/tree.svg"
 
 </script>
 
 
-<div class="bg-red-500">NON</div>
+<div class="bg-[#FFFFF6] h-screen">
+    <div class="bg-[#ACE1AF] h-[150px] flex items-center">
+        <div class="text-center w-full text-3xl">Calculateur</div>
+    </div>
+
+    <img class="fixed bottom-0 z-0 right-100 w-[100px]" src={Tree}>
+
+    <div class="grid mx-10 my-5">
+        <div>
+            <Search></Search>
+        </div>
+
+        <div>
+            <Results></Results>
+        </div>
+    </div>
+
+    
+
+</div>
